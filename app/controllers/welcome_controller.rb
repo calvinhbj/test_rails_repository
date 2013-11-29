e4c6813b3ebffaa725ebc61afb92c7555b94dbed
calvinhbj <bingjiu.he@163.com>
2013-11-29 16:52:42 +0800

    test_commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+
+class WelcomeController < ApplicationController
+  def say
+    
+  end
+  
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsTest::Application.routes.draw do
+  root :to => "welcome#index"
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
