4c69b789a0c216eadadc604c9bbc9d9e4cac7c47
calvinhbj <bingjiu.he@163.com>
2013-12-04 17:21:16 +0800

    test

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,19 @@
+# encoding: utf-8 
 module MembersHelper
+   SEX = [['男', 1], ['女', 0]]
+  def sex_list
+    SEX
+  end
+  def sex_name id
+    item = SEX.detect do |data|
+      data[1] == id
+    end
+    item ? item[0] : '未填写'
+  end
+
+  def age_list
+    1.upto(100).map do |d|
+      [d, d]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203063726) do
+ActiveRecord::Schema.define(version: 20131204071736) do
 
   create_table "activities", force: true do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20131203063726) do
     t.integer  "combo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sex"
   end
 
   create_table "people", force: true do |t|
